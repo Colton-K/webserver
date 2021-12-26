d72c131fb12b7d89bc890fc4b6544909378b8888
Colton Kammes <ckammes@nd.edu>
2021-12-26 10:24:47 -0500

    added service to github repo

--- newHub/setup.sh
+++ newHub/setup.sh
@@ -1,7 +1,10 @@
 # installs necessary python libraries to run the server
 sudo apt-get update
-sudo apt-get upgrade qq
+sudo apt-get upgrade
+
+sudo apt-get install python3-pip
 
 sudo python3 -m pip install flask
+sudo python3 -m pip install websockets
 
-sudo python3 -m pip install serial
+sudo cp webserver.service /etc/systemd/system/webserver.service
